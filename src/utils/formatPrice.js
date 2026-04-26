c1af1e1297ceacc368985b769d67616fb5cdd085
MartuOrtizNovero <martuortiznov@gmail.com>
2026-04-26 12:54:45 +0200

    fix: formatPrice

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -2,6 +2,7 @@ export const formatPrice = (price) => {
   return new Intl.NumberFormat("es-ES", {
     style: "currency",
     currency: "EUR",
+    currencyDisplay: "code",
     maximumFractionDigits: 0,
   }).format(price);
 };
